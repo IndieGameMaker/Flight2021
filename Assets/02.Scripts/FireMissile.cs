388998091eca6fb590659cb60f336c67b9da40f1
Lee Jae Hyun <myleje@gmail.com>
2021-04-23 10:48:51 +0900

    .

---
+++ Assets/02.Scripts/FireMissile.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireMissile : MonoBehaviour
+{
+    public GameObject missile;
+
+    void Fire()
+    {
+        Instantiate(missile,
+                    transform.position,
+                    Quaternion.LookRotation(transform.forward));
+    }
+
+}
