9c92e033f4ab965cd86f45550e2c068a501ce066
Lee Jae Hyun <myleje@gmail.com>
2021-04-23 09:51:19 +0900

    .

---
+++ Assets/02.Scripts/UfoCtrl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UfoCtrl : MonoBehaviour
+{
+    private Transform fighterTr;
+    private Transform ufoTr;
+
+    void Start()
+    {
+        fighterTr = GameObject.FindGameObjectWithTag("FLIGHT").GetComponent<Transform>();
+        ufoTr = GetComponent<Transform>();        
+    }
+
+    void LateUpdate()
+    {
+        //벡터 계산 (비행기의좌표 - UFO좌표)
+        Vector3 dir = fighterTr.position - ufoTr.position;
+        //벡터의 각도를 계산(쿼터니언 Quaternion)
+        Quaternion rot = Quaternion.LookRotation(dir);
+
+        ufoTr.rotation = Quaternion.Slerp(ufoTr.rotation,
+                                          rot,
+                                          Time.deltaTime * 5.0f);
+    }
+}
